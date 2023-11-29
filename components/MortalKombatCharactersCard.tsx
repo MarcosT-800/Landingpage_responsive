44f75c9a7a8ee5a9bd88507813c08e00f96f8c71
Marcos Alves <marcosalvesjr007@gmail.com>
2023-11-29 16:33:11 +0000

    style: largura_dos_cards

--- components/MortalKombatCharactersCard.tsx
+++ components/MortalKombatCharactersCard.tsx
@@ -22,17 +22,22 @@ const MortalKombatCharacterCard: React.FC<MortalKombatCharacterCardProps> = ({ c
       <div className="character-card__content mb-4">
         <h2 className="text-xl font-bold">{character.name}</h2>
       </div>
-      
+
       <div className='relative w-full h-40 mb-4 overflow-hidden rounded-md'>
-        <div className='aspect-w-4 aspect-h-5'>
-          <Image
-            src={character.image} // Updated path
-            alt={`${character.name} model`}
-            layout="fill" // Use layout="fill" to fill the parent container
-            objectFit="cover" // Maintain the aspect ratio and cover the container
-          />
-        </div>
-      </div>
+  <div className='aspect-w-4 aspect-h-5'>
+    <Image
+      src={character.image} // Caminho atualizado
+      alt={`${character.name} model`}
+      width={250} // Largura ajustada para 50 pixels
+      height={250 * (5 / 4)} // Mantenha a proporção
+      objectFit="cover" // Mantenha a proporção e cubra o contêiner
+    />
+  </div>
+</div>
+
+
+
+
 
       <div className='flex items-center justify-between'>
         <div className='flex flex-col items-center text-gray-700'>
@@ -55,7 +60,7 @@ const MortalKombatCharacterCard: React.FC<MortalKombatCharacterCardProps> = ({ c
             title='View More'
             containerStyles='w-full py-2 rounded-full bg-primary-red hover:bg-red-700 text-white text-sm font-bold'
             rightIcon='/right-arrow.svg'
-            // Add your custom click handler if needed
+          // Add your custom click handler if needed
           />
         </div>
       </div>
